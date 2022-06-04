dbed3a93f1612421eb46c219c5b23e55d3a911f8
marquezcode <gerardmarquezrodriguez@gmail.com>
2022-06-04 19:51:30 +0200

    added material icons and buttons
    upload icon on button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import UploadIcon from '@mui/icons-material/Upload';
 
 function App() {
   return (
     <div>
-      <Typography variant="h4" align="center">
+      <Typography variant="h4" align="center" gutterBottom>
         Bienvenido a DDrop
       </Typography>
-      <Typography align="center">
+      <Typography color="grey" align="center">
         Para subir tus archivos de forma simple a drive,<br></br>
         puedes hacer Login a traves de Google.
       </Typography>
+      <Button 
+       variant="contained"
+       color="primary"
+       align="center"
+       startIcon={<UploadIcon />}
+       disableElevation
+      >
+        Subir archivos
+      </Button>
       <nav
         style={{
           borderBottom: "solid 1px",
