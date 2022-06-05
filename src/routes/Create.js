4543ef45f88f7f9c5e945b367e45cac706854909
marquezcode <gerardmarquezrodriguez@gmail.com>
2022-06-05 11:18:40 +0200

    test vercel

--- src/routes/Create.js
+++ src/routes/Create.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Create() {
   return (
     <div>
-      <h1>Create Page</h1>
+      <h1>Create Page!</h1>
       <Link to="/notes">Notes</Link>
     </div>
   )
